7da4bc2344f84d1c0c8e0810f4766041fbadf605
Israel Garza <israel.gigb@gmail.com>
2018-11-22 12:56:04 -0600

    Avance personal y en equipo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cont=1;
 var contRooms=1;
 var contGlobal=0;
 var aux="";
-var rolGlobal="";
+//var rolGlobal="";
 var util = require("util");
 users=[];
 connections=[];
@@ -54,138 +54,173 @@ io.sockets.on('connection',function(socket){
     //var board=io.sockets.adapter.rooms[socket.room].board;
     //console.log(io.sockets.adapter.rooms[socket.room].board);
 
-    if(rolAux==1)
+    /*if(rolAux==1)
     rolGlobal=2;
     else {
-      rolGlobal=1;
-    }
-    contGlobal++;
-
-    if(contGlobal==10){
-      socket.to(socket.room).emit('bye');
-    }
-    else {
-      if(colAux<=7&&colAux>=1){
-        if(chkWinner(io.sockets.adapter.rooms[socket.room].board)==0)
-      io.in(socket.room).emit('move',rol,col,"El cliente "+rol+" envió "+col);
-    }
-
-  }
-
-
-  });
-
-  //enviar mensaje
-  socket.on('send message',function(data){
-    //io.sockets.emit('new message',{msg:data});
-    socket.to(socket.room).emit('message', {msg:data.msg});
-  });
-
-  //enviar tablero del cuarto
-  socket.on('send board',function(data){
-    socket.emit('board',{tablero:io.sockets.adapter.rooms[socket.room].board});
-  })
-
-  //enviar oponente al oponente
-  socket.on('send opponent',function(data){
-    //console.log(socket.username+' pide oponente');
-    socket.to(socket.room).emit('opponent', {opponent:socket.username});
-  })
-
-  //enviar cuarto en el que está el cliente que lo pide
-  socket.on('sendRoom',function(){
-
-    if(contRooms%2==1){
-      contRooms++;
-      aux="room"+contRooms/2;
-      socket.rol=1;
-    }
-    else {
-      aux="room"+contRooms/2;
-      contRooms++;
-      socket.rol=2;
-    }
-    socket.join(aux);
-    socket.room=aux;
-
-    //console.log("cuarto para "+socket.rol);
-
-    //prueba de guardar arreglo de juego como variable del room
-    /*if(aux=="room1"){
-      io.sockets.adapter.rooms[aux].board =
-      [['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-']];
-    }
-    else {
-      io.sockets.adapter.rooms[aux].board =
-      [['Y','E','S','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-'],
-      ['-','-','-','-','-','-','-']];
-    }*/
-
-    /*socket.join(aux, () => {
-    let rooms = Object.keys(socket.rooms);
-    socket.room=rooms[1];
-    aux=socket.room;
-
-    //console.log(rooms[1]); // [ <socket.id>, 'room 237' ]
-  });*/
-
-  io.sockets.adapter.rooms[aux].board =
-  [[0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0]];
-
-  socket.emit('getRoom',socket.rol,aux);
-
-  if(io.sockets.adapter.rooms[aux].length==2){
     rolGlobal=1;
-    socket.to(socket.room).emit('ready',rolGlobal);
-    //io.in(socket.room).emit('ready',1);
   }
-});
+  contGlobal++;*/
 
-function chkLine(a,b,c,d) {
-    // Check first cell non-zero and all cells match
-    return ((a != 0) && (a ==b) && (a == c) && (a == d));
+
+
+  /*if(contGlobal==10){
+  socket.to(socket.room).emit('bye');
+}
+else {*/
+if(colAux<=7&&colAux>=1){
+
+var tablero=io.sockets.adapter.rooms[socket.room].board;
+  col--;
+        for(var i=5; i>=0; i--){
+          if(tablero[i][col]==0){
+        tablero[i][col]=rol;
+          break;
+        }
+        }
+        col++;
+
+io.sockets.adapter.rooms[socket.room].board=tablero;
+
+console.log(tablero);
+
+  var resp=checarGanador(io.sockets.adapter.rooms[socket.room].board);
+  if(resp==0){
+
+  io.in(socket.room).emit('move',rol,col,"El cliente "+rol+" envió "+col);
+}
+  else{
+    console.log("ganó: "+resp+" del juego "+socket.room);
+    io.in(socket.room).emit('result',rol,col,"ganó: "+resp);
+    //}
+
+  }
+
+}
+else{
+  //persona perdio, checar
+  console.log("perdió: "+rol);
+  //io.in(socket.room).emit('result',rol,col,"perdió: "+rol);
+  //}
+
 }
 
-function chkWinner(bd) {
-    // Check down
-    for (r = 0; r < 3; r++)
-        for (c = 0; c < 7; c++)
-            if (chkLine(bd[r][c], bd[r+1][c], bd[r+2][c], bd[r+3][c]))
-                return bd[r][c];
 
-    // Check right
-    for (r = 0; r < 6; r++)
-        for (c = 0; c < 4; c++)
-            if (chkLine(bd[r][c], bd[r][c+1], bd[r][c+2], bd[r][c+3]))
-                return bd[r][c];
+});
 
-    // Check down-right
-    for (r = 0; r < 3; r++)
-        for (c = 0; c < 4; c++)
-            if (chkLine(bd[r][c], bd[r+1][c+1], bd[r+2][c+2], bd[r+3][c+3]))
-                return bd[r][c];
+//enviar mensaje
+socket.on('send message',function(data){
+  //io.sockets.emit('new message',{msg:data});
+  socket.to(socket.room).emit('message', {msg:data.msg});
+});
 
-    // Check down-left
-    for (r = 3; r < 6; r++)
-        for (c = 0; c < 4; c++)
-            if (chkLine(bd[r][c], bd[r-1][c+1], bd[r-2][c+2], bd[r-3][c+3]))
-                return bd[r][c];
+//enviar tablero del cuarto
+socket.on('send board',function(data){
+  socket.emit('board',{tablero:io.sockets.adapter.rooms[socket.room].board});
+})
 
-    return 0;
+//enviar oponente al oponente
+socket.on('send opponent',function(data){
+  //console.log(socket.username+' pide oponente');
+  socket.to(socket.room).emit('opponent', {opponent:socket.username});
+})
+
+//enviar cuarto en el que está el cliente que lo pide
+socket.on('sendRoom',function(){
+
+  if(contRooms%2==1){
+    contRooms++;
+    aux="juego"+contRooms/2;
+    socket.rol=1;
+  }
+  else {
+    aux="juego"+contRooms/2;
+    contRooms++;
+    socket.rol=2;
+  }
+  socket.join(aux);
+  socket.room=aux;
+
+  //console.log("cuarto para "+socket.rol);
+
+  //prueba de guardar arreglo de juego como variable del room
+  /*if(aux=="room1"){
+  io.sockets.adapter.rooms[aux].board =
+  [['-','-','-','-','-','-','-'],
+  ['-','-','-','-','-','-','-'],
+  ['-','-','-','-','-','-','-'],
+  ['-','-','-','-','-','-','-'],
+  ['-','-','-','-','-','-','-'],
+  ['-','-','-','-','-','-','-']];
+}
+else {
+io.sockets.adapter.rooms[aux].board =
+[['Y','E','S','-','-','-','-'],
+['-','-','-','-','-','-','-'],
+['-','-','-','-','-','-','-'],
+['-','-','-','-','-','-','-'],
+['-','-','-','-','-','-','-'],
+['-','-','-','-','-','-','-']];
+}*/
+
+/*socket.join(aux, () => {
+let rooms = Object.keys(socket.rooms);
+socket.room=rooms[1];
+aux=socket.room;
+
+//console.log(rooms[1]); // [ <socket.id>, 'room 237' ]
+});*/
+
+//console.log(io.sockets.adapter.rooms);
+
+io.sockets.adapter.rooms[aux].board =
+[[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0]];
+io.sockets.adapter.rooms[aux].rolCuarto=1;
+
+socket.emit('getRoom',socket.rol,aux);
+
+if(io.sockets.adapter.rooms[aux].length==2){
+
+  socket.to(socket.room).emit('ready',1);
+  //io.in(socket.room).emit('ready',1);
+}
+});
+
+function checarLinea(a,b,c,d) {
+  //checa 4 en linea, la primera no puede ser 0
+  return ((a != 0) && (a ==b) && (a == c) && (a == d));
+}
+
+function checarGanador(m) {
+  //checar vertical
+  for (r = 0; r < 3; r++)
+  for (c = 0; c < 7; c++)
+  if (checarLinea(m[r][c], m[r+1][c], m[r+2][c], m[r+3][c]))
+  return m[r][c];
+
+  //checar horizontal
+  for (r = 0; r < 6; r++)
+  for (c = 0; c < 4; c++)
+  if (checarLinea(m[r][c], m[r][c+1], m[r][c+2], m[r][c+3]))
+  return m[r][c];
+
+  //checar diagonal de arriba a abajo
+  for (r = 0; r < 3; r++)
+  for (c = 0; c < 4; c++)
+  if (checarLinea(m[r][c], m[r+1][c+1], m[r+2][c+2], m[r+3][c+3]))
+  return m[r][c];
+
+  //checar diagonal de abajo hacia arriba
+  for (r = 3; r < 6; r++)
+  for (c = 0; c < 4; c++)
+  if (checarLinea(m[r][c], m[r-1][c+1], m[r-2][c+2], m[r-3][c+3]))
+  return m[r][c];
+
+  return 0;
 }
 
 })
